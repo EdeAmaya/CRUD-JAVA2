bc03a3e05254418b1db5a3937748aacb9428c63d
EdeAmaya <edenilsonamaya10@gmail.com>
2024-08-14 19:20:06 -0600

    CRUD GUARDAR

---
+++ CRUD/src/Controlador/ctrlProducto.java
@@ -0,0 +1,49 @@
+
+package Controlador;
+
+import Modelo.Producto;
+import Vista.frmProductos;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+
+
+public class ctrlProducto implements MouseListener{
+    
+    private frmProductos vista;
+    private Producto modelo;
+    
+    public ctrlProducto(frmProductos Vista, Producto Modelo){
+        this.vista = Vista;
+        this.modelo = Modelo;
+        vista.btnGuardar.addMouseListener(this);
+               
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+       if(e.getSource() == vista.btnGuardar){
+       modelo.setNombre(vista.txtNombre.getText());
+       modelo.setPrecio(Double.parseDouble( vista.txtPrecio.getText()));
+       modelo.setCategoria(vista.txtCategoria.getText());
+       modelo.Guardar();
+       }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+    }
+    
+}
---
+++ CRUD/src/Modelo/Producto.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+import java.sql.*;
+import java.util.UUID;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Producto {
+    String UUID_producto;
+    String nombre;
+    double precio;
+    String categoria;
+
+    public String getUUID_producto() {
+        return UUID_producto;
+    }
+
+    public void setUUID_producto(String UUID_producto) {
+        this.UUID_producto = UUID_producto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+    
+    public void Guardar() {
+        //Creamos una variable igual a ejecutar el método de la clase de conexión
+        Connection conexion = ClaseConexion.getConexion();
+        try {
+            //Creamos el PreparedStatement que ejecutará la Query
+            PreparedStatement addProducto = conexion.prepareStatement("INSERT INTO tbProducto(UUID_producto, Nombre, precio, categoria) VALUES (?, ?, ?, ?)");
+            //Establecer valores de la consulta SQL
+            addProducto.setString(1, UUID.randomUUID().toString());
+            addProducto.setString(2, getNombre());
+            addProducto.setDouble(3, getPrecio());
+            addProducto.setString(4, getCategoria());
+ 
+            //Ejecutar la consulta
+            addProducto.executeUpdate();
+ 
+        } catch (SQLException ex) {
+            System.out.println("este es el error en el modelo:metodo guardar " + ex);
+        }
+    }
+}
